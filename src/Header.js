01e318bd53259da2848c174821e03c611c816036
reem-alqurm <ralqurm@gmail.com>
2021-06-17 15:57:57 +0300

    still working

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './header.scss';
-
+import { Link } from 'react-router-dom';
 const Header = ()=>{
     return(
         <header>
         <h1>RESTy</h1>
+        <nav>
+        <Link activeClassName="selected" to="/">Home</Link>
+        <Link activeClassName="selected" to="/history">History</Link>
+        <Link activeClassName="selected" to="/help">Help</Link>
+      </nav>
       </header>
     )
 }
--- src/Result.js
+++ src/Result.js
@@ -5,12 +5,15 @@ import ReactJson from "react-json-view";
 
 class Result extends React.Component {
   render() {
+    console.log(this.props.data.headers);
     return (
+      
       <>
         <div id="mainp">
           {/* <JSONPretty id="json-pretty" src={this.props} /> */}
           {/* <ReactJson id="json-pretty" src={this.props.results.headers} /> */}
-          
+          <ReactJson id="json-pretty" src={this.props.data.headers} />
+
           <ReactJson id="json-pretty" src={this.props} />
 
 
